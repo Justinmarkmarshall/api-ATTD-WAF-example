261c55229198f477fe9a1cffbcd944783762e484
Justin Marshall <justinmarkmarshall1@gmail.com>
2022-12-11 21:19:29 +0000

    improve comments

--- FileImport.AcceptanceTests/ImportController/ImportControllerTests.cs
+++ FileImport.AcceptanceTests/ImportController/ImportControllerTests.cs
@@ -15,18 +15,9 @@ namespace FileImport.AcceptanceTests.ImportController;
 
 //I have to create a Marker, which is an empty interface to mark the assembly
 //it is an interface in the Api I am testing called IApiMarker
-//IClassFixture means only one api sun up for all tests in the class
-//this method below is taken from Nicks course
-//public class ImportControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
-//this method below uses docs HERE https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-7.0
-// public class ImportControllerTests : WebApplicationFactory<Program>
-//try with just the normal instantiation without ClassFixture (2 below)
-//get constructor parameters did not have matching fixture data error
-//public class ImportControllerTests : WebApplicationFactory<IApiMarker>
+//IClassFixture means only one api news up for all tests in the class
 public class ImportControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
 {
-    //old way would be to new up a HttpClint, but we are taking from the WAF
-   
     private readonly HttpClient _httpClient;
     private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
     {
@@ -34,25 +25,22 @@ public class ImportControllerTests : IClassFixture<WebApplicationFactory<IApiMar
         IgnoreNullValues = true,
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
-
-    // private readonly WebApplicationFactory<Program> _factory;
+    
     public ImportControllerTests(WebApplicationFactory<IApiMarker> appFactory)
     {
-        //Nix way
-        //_httpClient = appFactory.CreateClient();
         _httpClient = appFactory.CreateClient();
     }
 
     //Integration test
-    //this succeeds in hitting the breakpoint in the controller
     [Fact]
     public async Task HitTheController()
     {
         var response = await _httpClient.GetAsync("import/");
-        
-        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        response.EnsureSuccessStatusCode(); 
     }
-    //so this can infact be turned into an acceptance test, the question is, if I run multiple feature files will there be multiple different instances of the API instantiated
+    //so this can infact be turned into an acceptance test, the question is,
+    //if I run multiple feature files will there be multiple different instances of the API instantiated
+    //i think so, but different files can be loaded in easily enough from the ACP project
     [Fact]
     public async Task ImportTheFileShouldFail()
     {
@@ -78,8 +66,7 @@ public class ImportControllerTests : IClassFixture<WebApplicationFactory<IApiMar
         
         importControllerResponse?.Success.Should().BeFalse();
     }
-
-    //does this create in the actual project file directory?
+    
     [Fact]
     public async Task ImportTheFileShouldPass()
     {
@@ -106,14 +93,9 @@ public class ImportControllerTests : IClassFixture<WebApplicationFactory<IApiMar
     public async Task ShouldReturnTheCreatedFile()
     {
         var location = "\\..\\..\\..\\Files\\ImportTextAcpTest.txt";
-        // string readFile = $"location={location}";
-        //var response = await _httpClient.GetAsync($"import/confirm/{location.ToString()}");
         var response = await _httpClient.GetAsync($"import/confirm?location={location}");
         
-        response.EnsureSuccessStatusCode(); // Status Code 200-299
-
-        var text = await response.Content.ReadAsStringAsync();
-        // text.Should().Contain("Unique Id");
+        response.EnsureSuccessStatusCode(); 
     }
     
 }
